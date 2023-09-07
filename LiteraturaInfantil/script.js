689c3fc53ff9362f54e664e605bd125aad3e5dab
Rodolfo Malheiros <125034296+RodolfoRMalheiros@users.noreply.github.com>
2023-09-07 15:54:58 -0300

    Update script.js
    Melhorias 15:53 07/09/2023

--- LiteraturaInfantil/script.js
+++ LiteraturaInfantil/script.js
@@ -39,3 +39,55 @@ function toggleSummary(bookId) {
         summary.style.display = "none";
     }
 }
+
+function toggleSummary(bookId) {
+    const summaries = document.querySelectorAll(".summary");
+    summaries.forEach(function (summary) {
+        if (summary.id === `${bookId}-summary`) {
+            // Se o summary corresponder ao livro clicado, alterna sua visibilidade.
+            if (summary.style.display === "block") {
+                summary.style.display = "none";
+            } else {
+                summary.style.display = "block";
+            }
+        } else {
+            // Caso contrário, fecha todas as outras summaries.
+            summary.style.display = "none";
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const loadMoreBtn = document.getElementById("loadMoreBtn");
+    const booksContainer = document.querySelector(".books-container");
+    const books = document.querySelectorAll(".book");
+    const booksPerPage = 3; // Número de livros para exibir inicialmente
+    let currentPage = 1;
+
+    // Função para mostrar os próximos livros
+    function showNextBooks() {
+        const startIndex = (currentPage - 1) * booksPerPage;
+        const endIndex = startIndex + booksPerPage;
+
+        books.forEach((book, index) => {
+            if (index >= startIndex && index < endIndex) {
+                book.style.display = "block";
+            } else {
+                book.style.display = "none";
+            }
+        });
+
+        currentPage++;
+
+        // Ocultar o botão "Carregar Mais" quando todos os livros foram exibidos
+        if (endIndex >= books.length) {
+            loadMoreBtn.style.display = "none";
+        }
+    }
+
+    // Event listener para o botão "Carregar Mais"
+    loadMoreBtn.addEventListener("click", showNextBooks);
+
+    // Exibir os primeiros livros
+    showNextBooks();
+});
